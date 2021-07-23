0d8bd0e0c2a35c5fc26d46e23a703768a194075f
Ramon Sanchez <ramonsanchezperez077@gmial.com>
2021-07-22 21:50:56 -0600

    ruta /ratingbarbershop/<int:barbershop_id>/<int:barbershop_rating> agregada correctamente

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, session, render_template, request, url_for, redirect, j
 from markupsafe import escape
 from helpers import loged_in
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import date
 
 from cs50 import SQL
 
 app = Flask(__name__)
 
 db = SQL("sqlite:///waitless.db")
+
 
 cookie_key = escape("b&#\\39;\\xd8c\\xb2C!.\\xbf\&#39;\\xfe[\\xd3 \\xb9\\x8b\\xe6\\x08K\\xde\\x068\\x98\\xfbO\\x05t\\x94R\\xbe\\x19&amp;V&#39;")
 app.secret_key = cookie_key
@@ -22,7 +24,6 @@ def about():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    
     if request.method == "GET":
         session.clear()
         return render_template("login.html")
@@ -81,15 +82,13 @@ def signin():
 @app.route('/')
 @loged_in
 def index():
-    """retorna el la pagina principal si esta logueadom, sino retorna la landing page"""
-    return render_template("index.html", key='', user=True)
+    return render_template("index.html", key='AIzaSyAoXq9RX6z2YVaWBKbg9aiFlp_SDeBYZCI', user=True)
 
 @app.route('/update')
 @loged_in
 def update():
-    """devuelve las coordenadas de las barberias a la vista del usuario con diferenciacion entre favoritas y normales"""
-    
-    # latitudes y logitudes que describen la pantalla que esta viendo el usuario.
+
+    #latitudes y logitudes que describen la pantalla que esta viendo el usuario.
     ne_lat, ne_lng, sw_lat, sw_lng = request.args.get("nelat"), request.args.get("nelgn"), request.args.get("swlat"), request.args.get("swlgn")
 
 
@@ -97,7 +96,6 @@ def update():
 
     # TODO: hacerle un order by rating para que te salgan las barberias mas relevantes.
     # seleccionando todas las barberias en la vista del mapa que no son las favoritas del usuario.
-    # el error de esta query me va a matar. casi me volvio mas loco.
     barbershops = db.execute("""SELECT p_barbershop_lat AS lat, p_barbershop_lng AS lng, p_barbershop_id AS id,
                              p_barbershop_name AS name
                              FROM place_barbershop LEFT JOIN favourite_place_barbershop
@@ -130,8 +128,11 @@ def update():
 @app.route('/infobarbershop/<int:barbershop_id>', methods=["GET", "POST"])
 @loged_in
 def info_barbershop(barbershop_id):
-    """devuelve informacion sobre la barberia seleccionada"""
-    # seis ultimos registros de la concurrencia de una barberia.
+
+    Barbershop_rating = db.execute("""SELECT avg(r_p_barbershop_rating) AS rating FROM rating_place_barbershop
+                                   where r_p_barbershop_place = :barbershop""", barbershop=barbershop_id)
+
+    # 6 ultimos registros de la concurrencia de una barberia.
     barbershop_concurrency_info = db.execute("""SELECT c_h_barbershop_concurrency AS concurrency,
                              c_h_barbershop_time AS date
                              FROM place_barbershop inner join concurrency_history_barbershop
@@ -148,17 +149,21 @@ def info_barbershop(barbershop_id):
                           WHERE f_p_barbershop_cod = :cod""", cod="{0}{1}".format(session["user_id"], barbershop_id)))
 
     # construyendo el objeto de respuesta.
-    barbershop_info = {"concurrency": barbershop_concurrency_info, "direccion": barbershop_direccion_info[0]["direccion"], "is_liked": is_liked}
+    barbershop_info = {"concurrency": barbershop_concurrency_info,
+                        "direccion": barbershop_direccion_info[0]["direccion"],
+                        "is_liked": is_liked,
+                        "rating": Barbershop_rating
+
+                      }
 
     return jsonify(barbershop_info)
 
 @app.route('/like/<int:barbershop_id>', methods=["DELETE", "POST"])
 @loged_in
 def like(barbershop_id):
-    """setea una barberia como favorita de un usuatio especifico"""
 
     if request.method == "POST":
-
+        # que pobresa ese SQL. Me doy pena. Ya se como hacerlo bien almenos.
         try:
             db.execute("""INSERT INTO favourite_place_barbershop (f_p_barbershop_user, f_p_barbershop_place, f_p_barbershop_cod)
                        VALUES(:user_id, :barbershop_id, :barbershop_cod)""", user_id=session["user_id"], barbershop_id=barbershop_id, barbershop_cod="{0}{1}".format(session["user_id"], barbershop_id))
@@ -178,7 +183,6 @@ def like(barbershop_id):
 @app.route("/concurrency/<int:barbershop_id>", methods=["POST"])
 @loged_in
 def concurrency(barbershop_id):
-    """actualiza el valor de la concurrencia de una barbera especifica para una barberia especifica selecciomada por el usuario"""
 
     concurrency_value = request.form.get("concurrency")
 
@@ -188,3 +192,33 @@ def concurrency(barbershop_id):
     return "ok", 200
 
 
+@app.route("/ratingbarbershop/<int:barbershop_id>/<int:barbershop_rating>", methods=["POST"])
+@loged_in
+def ratingbarbershop(barbershop_id, barbershop_rating):
+    if request.method == "POST":
+
+        #tenes que ver el formato en el que va ese date now y el de sql lite para que la mierda sirva: Ya sirve la mierda.
+
+        #Hay dos formas de insertar estos ratings en la base de datos: Un partial Unique index o chequias si existe el registro de ese usuario dandole rating a
+        #esa barberia y lo actualizas, sino, obvio lo insertas. Ni idea cual es mas eficiente.
+        #negativo. Lo que hay que hacer aqui es una composite key con los dos valores. r_p_barbershop_place, r_p_barbershop_user
+
+        #buscando si ya le dio rating el usuario a la barberia.
+        already_rated = db.execute("""select * from rating_place_barbershop
+                                   where r_p_barbershop_user = :user AND r_p_barbershop_place = :barbershop"""
+                                   , barbershop=barbershop_id, user=session["user_id"])
+        #si ya le dio rating actualizar el valor del rating
+        if  len(already_rated) == 1:
+            db.execute("""UPDATE rating_place_barbershop set r_p_barbershop_rating=:rating 
+                       where r_p_barbershop_user=:user AND r_p_barbershop_place=:barbershop"""
+                       , rating=barbershop_rating, user=session["user_id"], barbershop=barbershop_id)
+            return "ok", 200
+        #si no le ha dado rating insertar el nuevo registro con el rating.
+        else:
+            db.execute("""INSERT INTO rating_place_barbershop
+                       (r_p_barbershop_place, r_p_barbershop_user, r_p_barbershop_rating, r_p_barbershop_time)
+                       values(:barbershop, :user, :rating, julianday('now')) """, barbershop=barbershop_id, user=session["user_id"], rating=barbershop_rating)
+
+        return "ok", 200
+
+app.run(ssl_context='adhoc')
--- static/script.js
+++ static/script.js
@@ -1,6 +1,15 @@
 let map
 
-let info = new google.maps.InfoWindow()
+// ESTO DE AQUI HAY QUE HACERLO RESPONSIVE.
+//anchura de la ventana, altura de la ventana. window width, window height.
+let ww = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+let wh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+//si el ancho de ventana es mayor que la altura el info window ocupa un tercio del ancho de la pantalla, sino la ocupa toda.
+let infolen = ww > wh ? ww/3 : ww
+
+//intancia de infowindow dimeniones especificas configuradas.
+let info = new google.maps.InfoWindow({ maxWidth: infolen, minWidth: infolen })
 
 markers = []
 
@@ -56,14 +65,17 @@ document.addEventListener('DOMContentLoaded', () => {
         styles: mapStyles
     };
 
+    // creando y configurando el mapa.
     map = new google.maps.Map(mainDiv, mapOptions);
 
+
+    //este evento se dispara solo una vez y lo hace cuando el mapa se termina de instanciar. (creo xd)
     google.maps.event.addListenerOnce(map, "idle", configure);
 
 
 })
 
-
+// esto esta requetemal optimizado pero ya se como componerlo (creo x2)
 function configure() {
     google.maps.event.addLister
 
@@ -81,6 +93,61 @@ function configure() {
             update();
         }
     });
+
+    google.maps.event.addListener(map, "click", () => {
+        if (info.getMap()  || info.getMap){
+            info.close()
+        }
+    })
+
+    // creando un Custom Control. ver: https://developers.google.com/maps/documentation/javascript/controls#ControlPositioning
+    let controlDiv = document.createElement("div")
+    controlDiv.innerHTML = `<button><img src="https://img.icons8.com/color/48/000000/gps-device.png"/></button>`
+    controlDiv.style.margin = "1em"
+    controlDiv.index = 1
+
+    //custom image del marker para la geolocalizacion.
+    var geolocationImage = {
+        url: `https://earth.google.com/images/kml-icons/track-directional/track-none.png`,
+        size: new google.maps.Size(40, 40),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 0),
+        scaledSize: new google.maps.Size(40, 40)
+    }
+
+    // instancia del marcador que representa la ubicacion del usuario.
+    var geolocationMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(0, 0),
+        map: map,
+        title: 'your Location',
+        icon: geolocationImage
+    });
+    // Evento click del button localizacion
+    controlDiv.addEventListener("click", ()=>{
+        if (navigator.geolocation){
+
+            // orservando la ubicacion del usuario.
+            navigator.geolocation.watchPosition((pos)=>{
+
+
+                // actualizando el marcador que representa la geolocalizacion del usuario.
+                geolocationMarker.setPosition(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude))
+
+            },
+            ()=>{
+                console.log("error al activar localizacion.")
+            },
+            {
+                enableHighAccuracy: true,
+                maximumAge: 0
+            })
+        } else {
+            alert("no se puede usar la geolocalizacion")
+        }
+    })
+
+    // agregando un custom control a la interfaz de usuarios de google maps.
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(controlDiv)
 
     update()
 }
@@ -102,7 +169,7 @@ function addMarker(marker_place) {
 
     var image = {
         url: url,
-        size: new google.maps.Size(100, 100),
+        size: new google.maps.Size(30, 30),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(0, 0),
         scaledSize: new google.maps.Size(30, 30)
@@ -130,10 +197,30 @@ function addMarker(marker_place) {
             //hay una manera mucho mas elegante de escribir esto sin el setTimeout pero ya lo hice asi mmmm.
             likeButtonColorClass = res.is_liked ? "btn-danger" : "btn-light"
 
-            info.setContent(`<div class="card text-white bg-success mb-3" style="width: 18rem;">
+            console.log(res)
+
+            info.setContent(`<div id="infowindow_card" class="card text-white bg-success mb-3">
                                 <div class="card-header text-center">${marker.getTitle()}</div>
+
                                 <div class="card-body">
-                                    <h5 class="card-title">Historial de concurrencia últimas 6 actualizaciones</h5>
+
+                                    <div class="rate" id = "starRaterBarbershop">
+                                        <input class = "iStar" type="radio" id="star5" name="rate" value="5" />
+                                        <label for="star5" title="text">5 stars</label>
+                                        <input class = "iStar" type="radio" id="star4" name="rate" value="4" />
+                                        <label for="star4" title="text">4 stars</label>
+                                        <input class = "iStar" type="radio" id="star3" name="rate" value="3" />
+                                        <label for="star3" title="text">3 stars</label>
+                                        <input class = "iStar" type="radio" id="star2" name="rate" value="2" />
+                                        <label for="star2" title="text">2 stars</label>
+                                        <input class = "iStar" type="radio" id="star1" name="rate" value="1" />
+                                        <label for="star1" title="text">1 star</label>
+                                    </div>
+                                    <P id="barbershop_rating" class="card-text mr-auto pr-auto mb-auto d-block">${res.rating[0].rating? res.rating[0].rating:`<p class="text-nowrap bd-highlight">No ratings</p>`}</p>
+
+                                    
+
+                                    <h5 class="card-title ">Historial de concurrencia últimas 6 actualizaciones</h5>
                                     <div class="bg-light mb-2 px-auto">
                                     <canvas id="myChart" width="100" height=80></canvas>
                                     </div>
@@ -152,6 +239,9 @@ function addMarker(marker_place) {
                                     </select>
 
                                     <button id="btnSaveConcurrency" class="btn btn-light btn-outline-info d-none mt-2">confirmar actualizacion</button>
+
+
+
 
                                     <p class="card-text mt-2">Direccion: ${res.direccion}</p>
                             </div>`)
@@ -227,12 +317,12 @@ function addMarker(marker_place) {
                             //cambiando el icon al de una barberia favorita.
                             let heartNewImage = {
                                         url: "https://img.icons8.com/cotton/64/000000/like--v3.png",
-                                        size: new google.maps.Size(100, 100),
+                                        size: new google.maps.Size(30, 30),
                                         origin: new google.maps.Point(0, 0),
                                         anchor: new google.maps.Point(0, 0),
                                         scaledSize: new google.maps.Size(30, 30)
                                     }
-
+                            marker.setIcon(null)
                             marker.setIcon(heartNewImage)
                             } else if (res != "already liked"){
 
@@ -258,7 +348,7 @@ function addMarker(marker_place) {
 
                         }).then((res)=>{
                             if (res == "ok"){
-                            //cambiando el icon a un aberberia normal.
+                            //cambiando el icon a una berberia normal.
                             let barbershopNewImage = {
                                         url: "https://img.icons8.com/ios-filled/50/000000/barbershop.png",
                                         size: new google.maps.Size(100, 100),
@@ -317,12 +407,12 @@ function addMarker(marker_place) {
                             if (data.length > 6){
                                 data.shift()
                             }
-                            //pusheando la hora de la actualizacion mas teciente en el array que contiene la hora en el eje y.
+                            //pusheando la hora de la actualizacion mas reciente en el array que contiene la hora en el eje y.
                             let newTime = new Date()
                             time.push(`${newTime.getHours()}:${newTime.getMinutes()}`)
                             //eliminando el primer elemento del array osea la hora de la actualizacion mas antigua.
                             //se elimina solo cuado el grafico muestre mas de 6 puntos
-                            // estos dos ifs son un poco redundantes porque se podrian convertir en uno solo, pero asi es mas explicativo.
+                            //estos dos if son un poco redundantes porque se podrian convertir en uno solo, pero asi es mas explicativo. Lo converti en 1
                             if (time.length > 6){
                                 time.shift()
                             }
@@ -333,6 +423,37 @@ function addMarker(marker_place) {
                         }
                     })
                 })
+                //Div que contiene las stars.
+                let starRater = document.getElementById("starRaterBarbershop")
+                // I don't know what is going on hehe.
+                // esto esta bien chapusero pero bueno es lo que hay hehe
+                starRater.addEventListener("click", (e)=>{
+                    //este evento se esta disparando dos veces porque hay un default behaviour en el input que la esta cangando y no se como
+                    //componerlo porque el codigo que lo genera es css y yo de eso no mucho.
+                    //y funciona porque el elemento que este repetido no tiene la propiedad value y devuelve undefined.
+                    let starValue = (e.target.value)
+
+
+                    // esto es lo mas chapusero que mi mente, hasta el dia de hoy, ha podido engendrar.
+                    if (starValue === undefined) return
+
+                    fetch(`ratingbarbershop/${marker_place.id}/${starValue}`, {
+                        method: "POST",
+                        mode: "same-origin"
+                    }).then((res)=>{
+                        console.log(res)
+
+                    })
+                    .catch(((err)=>{
+                        console.log(err)
+                    }))
+
+
+
+                })
+
+
+
 
             },1)
 
@@ -372,4 +493,3 @@ function update() {
     })
 
 };
-
